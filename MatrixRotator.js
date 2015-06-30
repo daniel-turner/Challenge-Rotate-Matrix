5b5a3468578aaa94cf15fc014315b3ab05dcf17d
danturne <danturne@yahoo.com>
2015-06-29 17:21:21 -1000

    completed, with changes to test to prevent variable colliding

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -18,23 +18,44 @@ function MatrixRotator(matrix){
 
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
-MatrixRotator.prototype.rotate = function(direction) {
+MatrixRotator.prototype.rotate = function(direction, radius) {
 
   if(direction !== Direction.CW && direction !== Direction.CCW) {
 
     throw new Error("MatrixRotator did not receive a valid direction.");
   }
 
+  if(radius === undefined) {
+
+    radius = 0;
+
+  } else if(!(radius > 0 && radius <= Math.floor(this.matrix.length / 2))) {
+
+    throw new RangeError("MatrixRotator received an invalid radius");
+  }
+
+  if(typeof radius !== "number") {
+
+    throw new TypeError("MatrixRotator did not receive a number as radius");
+  }
+
+  var center = Math.floor(this.matrix.length / 2);
+
+  var validStartIndex = center - radius;
+  var validEndIndex = center + radius - Math.ceil(center%2);
+  var invalidStartIndex = validStartIndex + 1;
+  var invalidEndIndex = validEndIndex - 1;
+
   switch(direction) {
 
     case Direction.CW:
 
-      rotateClockwise(this.matrix);
+      return rotateClockwise(this.matrix, radius);
       break;
 
     case Direction.CCW:
 
-      rotateCounterClockwise(this.matrix);
+       return rotateCounterClockwise(this.matrix, radius);
       break;
 
     default:
@@ -42,32 +63,97 @@ MatrixRotator.prototype.rotate = function(direction) {
       throw new Error("MatrixRotator could not perform rotation");
   };
 
-  function rotateClockwise(inMatrix) {
+  function isInOuterBox(indexI, indexJ) {
 
-    var tempColumns = populateColumns(inMatrix);
+    if( indexI >= validStartIndex &&
+        indexI <= validEndIndex &&
+        indexJ >= validStartIndex &&
+        indexJ <= validEndIndex) {
 
-    for(var i = 0; i < tempColumns.length; i++) {
+      return true;
 
-      inMatrix[i] = tempColumns[i].reverse();
+    } else {
+
+      return false;
     }
-
-    return inMatrix;
   };
 
-  function rotateCounterClockwise(inMatrix) {
+  function isInInnerBox(indexI, indexJ) {
+
+    if(invalidStartIndex > invalidEndIndex) {
+
+      return false;
+    }
+
+    if( indexI >= invalidStartIndex &&
+        indexI <= invalidEndIndex &&
+        indexJ >= invalidStartIndex &&
+        indexJ <= invalidEndIndex) {
+
+      return true;
+
+    } else {
+
+      return false;
+    }
+  };
+
+  function rotateClockwise(inMatrix, radius) {
 
     var tempColumns = populateColumns(inMatrix);
-
-    console.log(inMatrix);
+    var tempMatrix = [];
 
     for(var i = 0; i < tempColumns.length; i++) {
 
-      inMatrix[i] = tempColumns[tempColumns.length - 1 - i];
+      tempMatrix.push(tempColumns[i].reverse());
     }
 
-    // console.log(inMatrix);
+    if(radius === 0) {
 
-    return inMatrix;
+      return tempMatrix;
+    }
+
+    for(var i = 0; i < tempMatrix.length; i++) {
+
+      for(var j = 0; j < tempMatrix.length; j++) {
+
+        if(!isInOuterBox(i,j) || isInInnerBox(i,j)) {
+
+          tempMatrix[i][j] = inMatrix[i][j];
+        }
+      }
+    }
+
+    return tempMatrix;
+  };
+
+  function rotateCounterClockwise(inMatrix, radius) {
+
+    var tempColumns = populateColumns(inMatrix);
+    var tempMatrix = [];
+
+    for(var i = 0; i < tempColumns.length; i++) {
+
+      tempMatrix.push(tempColumns[tempColumns.length - 1 - i]);
+    }
+
+    if(radius === 0) {
+
+      return tempMatrix;
+    }
+
+    for(var i = 0; i < tempMatrix.length; i++) {
+
+      for(var j = 0; j < tempMatrix.length; j++) {
+
+        if(!isInOuterBox(i,j) || isInInnerBox(i,j)) {
+
+          tempMatrix[i][j] = inMatrix[i][j];
+        }
+      }
+    }
+
+    return tempMatrix;
   };
 
   function populateColumns(matrix) {
